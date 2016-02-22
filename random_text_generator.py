fd2f4c1d163220871f74c72241047e1b70ddbfcc
Alex Boklin <alex.boklin@gmail.com>
2016-02-22 15:40:04 +0300

    add writing to file functionality

--- random_text_generator.py
+++ random_text_generator.py
@@ -2,8 +2,8 @@ import random
 import string
 
 # wordlengths:       1 2 3 4 5 6 7
-# their frequencies: 1 2 3 4 3 2 1 
-wordlength_frequences = [1,2,2,3,3,3,4,4,4,4,5,5,5,6,6,7]   	
+# their frequencies: 1 2 3 4 3 2 1
+wordlength_frequences = [1,2,2,3,3,3,4,4,4,4,5,5,5,6,6,7]
 
 def generate_n_words(n):
 	for i in range(n):
@@ -17,7 +17,10 @@ def generate_n_words(n):
 
 def main(): 
 	words_to_generate = int(input("How many words do you need, playa?\n"))
-	print((' ').join(word for word in generate_n_words(words_to_generate)))
+	with open("random_words.txt", "a+") as output_file: # open for reading and writing, create is doesn't exist and append
+		new_line = ((' ').join(word for word in generate_n_words(words_to_generate)))
+		output_file.write(new_line + "\n")
 
 if __name__ == "__main__":
     main()  
+
